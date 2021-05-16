218401de2b4e3a9ddfb006844abd9978ae4884e5
Richard Harris <120225671@umail.ucc.ie>
2021-05-16 18:31:02 +0100

    First Commit

---
+++ Main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	Num := 0
+	EnteredNum := 0
+	Diff := 0
+	for {
+
+		fmt.Println("The current number is: ", Num, "/n")
+
+		fmt.Println("Please enter desired number")
+
+		fmt.Scanln(EnteredNum)
+
+		Diff = EnteredNum - Num
+
+		Num = Diff + Num
+
+	}
+}
